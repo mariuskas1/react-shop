20bbe08456dbe7b4263d2ce2257888bef868c7c0
Marius Kasparek <marius.kasparek@gmail.com>
2025-03-19 13:27:00 +0100

    basic shopping-cart logic

--- src/components/shopping-cart.jsx
+++ src/components/shopping-cart.jsx
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 
 
 class ShoppingCart extends Component {
-    state = {  } 
+    state = { 
+        items: [
+            {
+                amount:1,
+                name:'Tomaten',
+                price:4.9
+            },
+            {
+                amount:2,
+                name:'Äpfel',
+                price:4.9
+            }
+        ]
+     } 
+
     render() { 
         return <div>
             <h2>Warenkorb</h2>
+            { this.state.items.map(item => <div key={item.name}>{item.amount}x {item.name} </div>) }
         </div>;
     }
 }
